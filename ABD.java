92a12aa6cc9267ee430b63a2f1cc74f70c60eaf5
Yaseen970 <162338661+Yaseen970@users.noreply.github.com>
2024-03-05 20:35:46 +0530

    Add files via upload

---
+++ ABD.java
@@ -0,0 +1,20 @@
+package com.si.day2;
+
+public class ABD {
+	int a=10;
+	static int b=20;
+	
+
+	public static void main(String[] args) {
+		int c=30;
+		System.out.println(+c);
+		ABD obj=new ABD();
+		System.out.println(obj.a);
+		System.out.println(ABD.b);
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
