f4978da2c184d89d4cba42fe827b4eb5e47df5eb
Yaseen970 <162338661+Yaseen970@users.noreply.github.com>
2024-03-05 20:37:06 +0530

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,18 @@
+package com.si.day2;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		NormalClass n1=new NormalClass();
+		System.out.println(n1.a);
+		System.out.println(NormalClass.b);
+		n1.display();
+		NormalClass.display1();
+		
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ NormalClass.java
@@ -0,0 +1,15 @@
+package com.si.day2;
+
+public class NormalClass {
+	int a=10;
+	  static int b=20;
+	  String display() {
+		  return"Hello display";
+		  
+	  }
+		static String display1() {
+			return"Hello Static Display";
+		}
+
+
+}
